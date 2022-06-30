aeef1f32f36d96d36775505bf6182d273f7a1dd5
Abhiram Rao <73462883+aquantumreality@users.noreply.github.com>
2022-06-30 11:44:58 +0530

    Add files via upload

--- SigPro/fft.cpp
+++ SigPro/fft.cpp
@@ -1,28 +1,100 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <unordered_map>
+#define ll long long
+#define inf 9223372036854775807
+#define minf -9223372036854775808
+#define mod 1000000007
+
+//#define endl "\n"
+//#include "stdc++.h"
+
+
+using namespace std;
 using cd = complex<double>;
 const double PI = acos(-1);
 
+int reverse(int num, int lg_n) {
+    int res = 0;
+    for (int i = 0; i < lg_n; i++) {
+        if (num & (1 << i))
+            res |= 1 << (lg_n - 1 - i);
+    }
+    return res;
+}
+
 void fft(vector<cd> & a, bool invert) {
     int n = a.size();
-    if (n == 1)
-        return;
+    int lg_n = 0;
+    while ((1 << lg_n) < n)
+        lg_n++;
 
-    vector<cd> a0(n / 2), a1(n / 2);
-    for (int i = 0; 2 * i < n; i++) {
-        a0[i] = a[2*i];
-        a1[i] = a[2*i+1];
+    for (int i = 0; i < n; i++) {
+        if (i < reverse(i, lg_n))
+            swap(a[i], a[reverse(i, lg_n)]);
     }
-    fft(a0, invert);
-    fft(a1, invert);
 
-    double ang = 2 * PI / n * (invert ? -1 : 1);
-    cd w(1), wn(cos(ang), sin(ang));
-    for (int i = 0; 2 * i < n; i++) {
-        a[i] = a0[i] + w * a1[i];
-        a[i + n/2] = a0[i] - w * a1[i];
-        if (invert) {
-            a[i] /= 2;
-            a[i + n/2] /= 2;
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
         }
-        w *= wn;
+    }
+
+    if (invert) {
+        for (cd & x : a)
+            x /= n;
     }
 }
+
+vector<ll> multiply(vector<ll> const& a, vector<ll> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    ll n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (ll i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<ll> result(n);
+    for (ll i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+
+void solve(){
+    vector<ll> v1;
+    vector<ll> v2;
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+    v2.push_back(1);
+    v2.push_back(-1);
+    vector<ll> ans=multiply(v1,v2);
+    for(auto x:ans)
+    cout<<x<<" ";
+}
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    //cin>>t;
+    t=1;
+    while(t--) solve();
+} 
+ 
